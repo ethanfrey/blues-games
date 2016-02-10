86cf898f1bf7227abd1407e76d4c099d7901658d
Ethan <ethan@orderbird.com>
2016-02-10 17:05:46 +0100

    Add bluetooth scan

--- app/src/main/java/com/orderbird/ethanblue/MainActivity.java
+++ app/src/main/java/com/orderbird/ethanblue/MainActivity.java
@@ -1,7 +1,15 @@
 package com.orderbird.ethanblue;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -10,9 +18,9 @@ import java.util.List;
 
 /*
 TODO:
-http calls
-background services
-bluetooth queries
+http calls  -  volley  (for registration, notification of found)
+background services  ()
+push notification  - google service from web app
  */
 
 public class MainActivity extends AppCompatActivity {
@@ -20,6 +28,22 @@ public class MainActivity extends AppCompatActivity {
     ListView bluelist;
     ListAdapter adapter;
     List mData;
+    Button mButton;
+
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                String line = device.getName() + "\n" + device.getAddress();
+                if (! mData.contains(line)) {
+                    mData.add(line);
+                }
+                adapter.notifyDataSetChanged();
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,12 +51,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         bluelist = (ListView)findViewById(R.id.bluelist);
         mData = new ArrayList<String>();
-        for (int i = 0; i < 100; i++) {
-            mData.add("Child " + String.valueOf(i));
-        }
+        mData.add("<BLANK HEADER>");
         adapter = new ListAdapter(this, mData);
         bluelist.setAdapter(adapter);
 //        Toast.makeText(this, "Create Main Activity", Toast.LENGTH_SHORT).show();
+
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(mReceiver, filter);
+
+        mButton = (Button)findViewById(R.id.discovery);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                discover();
+            }
+        });
+    }
+
+    public void discover() {
+        boolean worked = BluetoothAdapter.getDefaultAdapter().startDiscovery();
+        if (! worked) {
+            Toast.makeText(this, "Bluetooth failed", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        unregisterReceiver(mReceiver);
+        super.onDestroy();
     }
 
     @Override
